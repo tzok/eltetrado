44fca49bfc6533536639b3bd8658977cc111010e
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2019-07-04 08:57:32 +0200

    Code reformat

--- mp.R
+++ mp.R
@@ -1,13 +1,14 @@
 #! /usr/bin/env Rscript
 
 # source: http://colorbrewer2.org/?type=qualitative&scheme=Paired&n=9
-default <- 'black'
-o_plus  <- '#1f78b4'
-o_minus <- '#a6cee3'
-n_plus  <- '#33a02c'
-n_minus <- '#b2df8a'
-z_plus  <- '#ff7f00'
-z_minus <- '#fdbf6f'
+o_plus      <- '#1f78b4'
+o_minus     <- '#a6cee3'
+n_plus      <- '#33a02c'
+n_minus     <- '#b2df8a'
+z_plus      <- '#ff7f00'
+z_minus     <- '#fdbf6f'
+default     <- 'gray'
+canonical   <- 'black'
 
 args = commandArgs(trailingOnly=TRUE)
 
@@ -30,6 +31,8 @@ helix1$col[which(helix1$value==3)]  <- n_plus
 helix1$col[which(helix1$value==4)]  <- n_minus
 helix1$col[which(helix1$value==5)]  <- z_plus
 helix1$col[which(helix1$value==6)]  <- z_minus
+helix1$col[which(helix1$value==7)]  <- default
+helix1$col[which(helix1$value==8)]  <- canonical
 
 helix2$col                          <- default
 helix2$col[which(helix2$value==1)]  <- o_plus
@@ -38,6 +41,8 @@ helix2$col[which(helix2$value==3)]  <- n_plus
 helix2$col[which(helix2$value==4)]  <- n_minus
 helix2$col[which(helix2$value==5)]  <- z_plus
 helix2$col[which(helix2$value==6)]  <- z_minus
+helix1$col[which(helix2$value==7)]  <- default
+helix1$col[which(helix2$value==8)]  <- canonical
 
 x<-attributes(fasta)
 x<-as.graphicsAnnot(x)
