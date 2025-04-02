1da56ef9d5c34440147f9695eff9ace3ddfa126a
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2025-04-02 14:55:14 +0200

    feat: Add test files for FR3D tetrad detection and analysis

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -14,7 +14,20 @@ def test_has_tetrad_with_dssr(capfd):
     assert out == "True\n"
 
 
-def test_has_tetrad_without_dssr(capfd):
+def test_has_tetrad_with_fr3d(capfd):
+    has_tetrad_cli(
+        [
+            "--input",
+            "tests/files/1v3p-assembly-1.cif.gz",
+            "--fr3d-txt",
+            "tests/files/1v3p-assembly-1.txt",
+        ]
+    )
+    out, err = capfd.readouterr()
+    assert out == "True\n"
+
+
+def test_has_tetrad(capfd):
     has_tetrad_cli(
         [
             "--input",
@@ -37,6 +50,21 @@ def test_eltetrado_with_dssr(capfd):
     )
     out, err = capfd.readouterr()
     with open("tests/files/2awe-assembly-1.out") as f:
+        assert out == f.read()
+
+
+def test_eltetrado_with_fr3d(capfd):
+    eltetrado_cli(
+        [
+            "--input",
+            "tests/files/2awe-assembly-1.cif.gz",
+            "--fr3d-txt",
+            "tests/files/2awe-assembly-1.txt",
+            "--no-image",
+        ]
+    )
+    out, err = capfd.readouterr()
+    with open("tests/files/2awe-assembly-1-fr3d.out") as f:
         assert out == f.read()
 
 
